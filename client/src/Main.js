c6e611057b0790de2d6f57e01e4e745ee236ac24
foozie3moons <matthew.bell.290@gmail.com>
2017-10-17 12:26:42 -0700

    finished basic form

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import Form from './Form'
+import React, {Component} from 'react';
+import BsForm from './BsForm'
 
 import { Grid } from 'react-bootstrap'
 import { Col } from 'react-bootstrap'
 
-function Main() {
+class Main extends Component {
 
-  return (
-    <Grid>
-      <Col md={8}>
-        <Form />
-      </Col>
-      <Col md={4}>
-        <div id='Section Goes Here'>
-        </div>
-      </Col>
-    </Grid>
-  )
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <Grid>
+        <Col md={6}>
+          <BsForm />
+        </Col>
+        <Col md={6}>
+          <div id='Section Goes Here'>
+          </div>
+        </Col>
+      </Grid>
+    )
+  }
 }
 
-export default Main;
+export default Main
