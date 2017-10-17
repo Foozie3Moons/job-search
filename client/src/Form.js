a7cecf6363e1641a48ebf8c5ba600144a6d8ff11
foozie3moons <matthew.bell.290@gmail.com>
2017-10-17 11:27:48 -0700

    added a basic bootstrap form

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { FormGroup } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
+import { ControlLabel } from 'react-bootstrap';
+import { HelpBlock } from 'react-bootstrap';
 
-const Form = () => {
+class Form extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      value: ''
+    }
+  }
+  getValidationState = () => {
+    const length = this.state.value.length;
+    if (length > 10) return 'success';
+    else if (length > 5) return 'warning';
+    else if (length > 0) return 'error';
+  }
 
-  return (
-    <form>
-      <label for='title'>Title</label>
-      <input type='text' placeholder='Title'></input>
-    </form>
-  )
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  render() {
+    return (
+      <form>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Working example with validation</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.value}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock>Validation is based on string length.</HelpBlock>
+        </FormGroup>
+      </form>
+    );
+  }  
 }
 
 export default Form;
