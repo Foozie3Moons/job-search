11d7c6739bc6d67a3167e8b50b4d951d8072cf07
chasedeger <cdeger88@gmail.com>
2017-10-17 10:49:40 -0700

    made sideBar

---
+++ client/src/NavBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    render() {
+      return (
+        <div className="dateApplied">
+
+        </div>
+        <div className="count">
+
+        </div>
+        <div className="personalNote">
+
+        </div>
+        <div className="jobLink">
+          
+        </div>
+      )
+    }
+  }
+}
+
+export default NavBar;
