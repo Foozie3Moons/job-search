9e714f58a9b6378867da22ee4ebdf51103b9db43
chasedeger <cdeger88@gmail.com>
2017-10-17 19:21:09 -0700

    finished personal note

--- client/src/SideBar.js
+++ client/src/SideBar.js
@@ -11,11 +11,17 @@ class SideBar extends Component {
     this.state = {
       linkArray: ['www.monster.com'],
       editMode: false,
-      editId: ''
+      editId: '',
+      noteArray: ['You can do this'],
+      noteEditMode: false,
+      noteEditId: ''
     };
     this.addNewLink = this.addNewLink.bind(this);
     this.editLink = this.editLink.bind(this);
     this.toggleEditMode = this.toggleEditMode.bind(this);
+    this.addNewNote = this.addNewNote.bind(this);
+    this.editNote = this.editNote.bind(this);
+    this.noteToggleEditMode = this.noteToggleEditMode.bind(this);
   }
   addNewLink(linkName) {
     var temp = this.state.linkArray;
@@ -23,27 +29,45 @@ class SideBar extends Component {
     // These this.setState calls require those bind() lines above
     this.setState({linkArray: temp});
   }
-
+  addNewNote(noteName) {
+    var temp = this.state.noteArray;
+    temp.push(noteName);
+    this.setState({noteArray: temp});
+  }
   // Method called by TaskList when the Edit link is clicked
   // Passed in via props
   toggleEditMode(linkId) {
-    console.log("App::toggleEditMode");
     this.setState({
       editMode: true,
       editId: linkId
+    });
+  }
+  noteToggleEditMode(noteId) {
+    this.setState({
+      noteEditMode: true,
+      noteEditId: noteId
     });
   }
 
   // Method called by TaskList when the Save Changes link is clicked
   // Passed in via props
   editLink(linkId, value) {
-    // console.log("in the editTask");
     var tempArr = this.state.linkArray;
     tempArr[linkId] = value;
     this.setState({
       linkArray: tempArr,
       editMode: false,
       editId: ''
+    });
+  }
+
+  editNote(noteId, value) {
+    var noteTempArr = this.state.noteArray;
+    noteTempArr[noteId] = value;
+    this.setState({
+      noteArray: noteTempArr,
+      noteEditMode: false,
+      noteEditId: ''
     });
   }
 
@@ -55,14 +79,18 @@ class SideBar extends Component {
         </div>
         <Count />
         <div className="PersonalNote">
-          personal note
+          <PersonalNote noteEditMode={this.state.noteEditMode}
+                  noteEditId={this.state.noteEditId}
+                  noteToggleEditMode={this.noteToggleEditMode}
+                  editNote={this.editNote}
+                  notes={this.state.noteArray} />
         </div>
         <div className="JobLink">
           <JobLink editMode={this.state.editMode}
                   editId={this.state.editId}
                   toggleEditMode={this.toggleEditMode}
-                  editTask={this.editTask}
-                  links={this.state.taskArray} />
+                  editLink={this.editLink}
+                  links={this.state.linkArray} />
         </div>
       </div>
     )
